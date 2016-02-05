e18a6021d4bfb0de012a29207a1277ad7ce36ab3
Maurício Maia <mauricio.maia@gmail.com>
2016-02-05 19:11:06 -0200

    reconfigure Sidekiq client

--- lib/gush.rb
+++ lib/gush.rb
@@ -33,14 +33,18 @@ module Gush
 
   def self.configure
     yield configuration
-    reconfigure_sidekiq_server
+    reconfigure_sidekiq
   end
 
-  def self.reconfigure_sidekiq_server
+  def self.reconfigure_sidekiq
     Sidekiq.configure_server do |config|
+      config.redis = { url: configuration.redis_url, queue: configuration.namespace}
+    end
+
+    Sidekiq.configure_client do |config|
       config.redis = { url: configuration.redis_url, queue: configuration.namespace}
     end
   end
 end
 
-Gush.reconfigure_sidekiq_server
+Gush.reconfigure_sidekiq
