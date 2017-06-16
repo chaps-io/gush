8fd9a79bf3f5b1a1820515043701bd12025f60aa
piotrek <piotrek@okonski.org>
2017-06-16 16:15:08 +0200

    Simplify incoming_payloads

--- lib/gush/worker.rb
+++ lib/gush/worker.rb
@@ -41,17 +41,14 @@ module Gush
     end
 
     def incoming_payloads
-      payloads = []
-      job.incoming.each do |job_name|
+      job.incoming.map do |job_name|
         job = client.load_job(workflow.id, job_name)
-        payloads << {
+        {
           id: job.name,
           class: job.klass.to_s,
           output: job.output_payload
         }
       end
-
-      payloads
     end
 
     def mark_as_finished
