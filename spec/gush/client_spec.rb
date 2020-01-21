7c89931dbb7747f679b5bbd231a3648990211940
Anirban Mukhopadhyay <anirbanmu@users.noreply.github.com>
2020-01-21 02:48:52 -0600

    Limit number of redis connections and fix ttl bug (#75)
    * Attempting a quick workaround for gush creating a massive number of redis connection.
    
    With this design, each thread has its own redis connection (which is replaced if for some reason
    a new redis url is seen (should be unlikely))
    
    * Set TTL on job key based on class not name (name includes uuid which doesn't exist in redis). Added tests to verify as well.
    
    * Clean up dependencies and fix travis CI

--- spec/gush/client_spec.rb
+++ spec/gush/client_spec.rb
@@ -95,12 +95,18 @@ describe Gush::Client do
   end
 
   describe "#expire_workflow" do
+    let(:ttl) { 2000 }
+
     it "sets TTL for all Redis keys related to the workflow" do
       workflow = TestWorkflow.create
 
-      client.expire_workflow(workflow, -1)
+      client.expire_workflow(workflow, ttl)
 
-      # => TODO - I believe fakeredis does not handle TTL the same.   
+      expect(redis.ttl("gush.workflows.#{workflow.id}")).to eq(ttl)
+
+      workflow.jobs.each do |job|
+        expect(redis.ttl("gush.jobs.#{workflow.id}.#{job.klass}")).to eq(ttl)
+      end
     end
   end
 
